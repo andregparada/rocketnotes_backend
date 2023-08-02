34e37be6d3ed3757226bee2c440ca3179241856f
André <andregparada@gmail.com>
2023-08-02 11:52:08 -0300

    validando usuário cadastrado

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,10 +1,17 @@
-const { response } = require("express");
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
 
 class SessionsController {
     async create(request, response) {
-        const { email, password } = request.body
+        const { email, password } = request.body;
 
-        return response.json({ email, password })
+        const user = await knex("users").where({ email }).first();
+
+        if(!user) {
+            throw new AppError("E-mail e/ou senha incorreta", 401)
+        }
+
+        return response.json(user);
     }
 }
 
