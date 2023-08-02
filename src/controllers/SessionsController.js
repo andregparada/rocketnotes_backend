f7cb4c4d16de9732934a4aef6442967ae2a4830b
André <andregparada@gmail.com>
2023-08-02 11:43:00 -0300

    rotas de autenticação

---
+++ src/controllers/SessionsController.js
@@ -0,0 +1,11 @@
+const { response } = require("express");
+
+class SessionsController {
+    async create(request, response) {
+        const { email, password } = request.body
+
+        return response.json({ email, password })
+    }
+}
+
+module.exports = SessionsController;
