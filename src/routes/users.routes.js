c258149bf89d08affe4c89f5255d161c599a2487
André <andregparada@gmail.com>
2023-08-03 11:37:44 -0300

    utilizando middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,23 +1,13 @@
-const { Router } = require('express')
+const { Router } = require('express');
 
-const UserController = require("../controllers/UserController")
+const UserController = require("../controllers/UserController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const userRoutes = Router()
 
 const userController = new UserController();
 
-userRoutes.post('/', userController.create)
-userRoutes.put('/:id', userController.update)
+userRoutes.post('/', userController.create);
+userRoutes.put('/', ensureAuthenticated, userController.update);
 
 module.exports = userRoutes
-
-
-// function myMiddleware(request, response, next) {
-//     console.log("Você passou pelo Middleware!")
-
-//     if(!request.body.isAdmin) {
-//         return response.json({ meesage: "user unauthorized" })
-//     }
-
-//     next()
-// }
