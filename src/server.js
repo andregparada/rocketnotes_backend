588f05732e63fa608039c1c6e48e916dca735bfa
André <andregparada@gmail.com>
2023-08-04 10:58:25 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -3,19 +3,19 @@ const migrationsRun = require('./database/sqlite/migrations');
 const AppError = require("./utils/AppError");
 const uploadConfig = require("./configs/upload");
 
+const cors = require("cors");
 const express = require('express');
+const routes = require('./routes');
 
-const routes = require('./routes')
+migrationsRun();
 
 const app = express();
-app.use(express.json())
+app.use(cors())
+app.use(express.json());
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes);
-
-migrationsRun();
-
 
 app.use((error, request, response, next) => {
     if(error instanceof AppError) {
