d3470f2516a98c631318c4671000fbe91037cb3a
André <andregparada@gmail.com>
2023-08-15 10:04:59 -0300

    configurando PM2

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 require("express-async-errors");
-const migrationsRun = require('./database/sqlite/migrations');
-const AppError = require("./utils/AppError");
+require("dotenv/config");
+
 const uploadConfig = require("./configs/upload");
 
-const cors = require("cors");
+const migrationsRun = require('./database/sqlite/migrations');
+const AppError = require("./utils/AppError");
+
 const express = require('express');
+const cors = require("cors");
 const routes = require('./routes');
 
 migrationsRun();
@@ -33,26 +36,5 @@ app.use((error, request, response, next) => {
     })
 })
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
-
-// app.get('/message/:id/:user', (request, response) => {
-//     const { id, user } = request.params
-
-//     response.send(`
-//         Id da mensagem: ${id}.
-//         Para o usuário: ${user}.
-//     `)
-// })
-
-// app.get('/users', (request, response) => {
-//     const { page, limit } = request.query
-
-//     response.send(`Página: ${page}. Mostrar: ${limit}`)
-// })
-
-// app.post('/users', (request, response) => {
-//     const { name, email, password} = request.body
-
-//     response.send(`Usuário: ${name}. E-Mail: ${email}. Senha ${password}`)
-// })
